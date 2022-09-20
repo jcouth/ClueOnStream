4e42059c7419a0ffd91f6b965073f8fe347a311e
Couth <craftcouth@gmail.com>
2022-09-20 20:43:58 -0300

    Fix: env variable

--- src/services/client.ts
+++ src/services/client.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const { BASE_URL } = process.env;
+const { REACT_APP_BASE_URL } = process.env;
 
 const api = axios.create();
 
-api.defaults.baseURL = BASE_URL;
+api.defaults.baseURL = REACT_APP_BASE_URL;
 
 // api.defaults.headers = {
 //   'Access-Control-Allow-Origin': '*',

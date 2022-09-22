24e061693c463a1cf5d7738a2735b31b6dc416b0
Couth <craftcouth@gmail.com>
2022-09-22 17:45:18 -0300

    Fix: shuffle helper to generic type

--- src/helpers/shuffleAndSlice.ts
+++ src/helpers/shuffleAndSlice.ts
@@ -1,5 +1,5 @@
-export const shuffleAndSlice = (array: any[], maxItems: number) => {
+export function shuffleAndSlice<T = any>(array: T[], maxItems: number) {
   const shuffled = [...array].sort(() => 0.5 - Math.random());
 
   return shuffled.slice(0, maxItems);
-};
+}

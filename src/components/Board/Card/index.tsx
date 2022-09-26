e941152296a38011335be868979e24e414667897
Couth <craftcouth@gmail.com>
2022-09-26 15:56:52 -0300

    Add: card component of board

---
+++ src/components/Board/Card/index.tsx
@@ -0,0 +1,55 @@
+import React, { memo, useState } from 'react';
+
+import { ReactComponent as ProfileCard } from '../../../assets/profile-card.svg';
+
+import { Card as CardProps } from '../../../interfaces/Card';
+
+import * as S from './styles';
+
+interface Props extends CardProps {
+  isStreamerTurn: boolean;
+  onOpen(id: CardProps['id'], type: CardProps['type']): void;
+}
+
+const Card: React.FC<Props> = ({
+  id,
+  title,
+  isOpen,
+  type,
+  isStreamerTurn,
+  onOpen,
+}) => {
+  const [shake, setShake] = useState<boolean>(false);
+
+  const handleOpen = () => {
+    if (isOpen || isStreamerTurn) {
+      if (!shake) {
+        setShake(true);
+      }
+    } else {
+      onOpen(id, type);
+    }
+  };
+
+  return (
+    <S.Container
+      isOpen={isOpen}
+      cardType={type}
+      onClick={handleOpen}
+      className={shake ? 'shake' : ''}
+      onAnimationEnd={() => setShake(false)}
+    >
+      <S.Header>
+        <S.Percentage visible={false}>
+          <S.PercentageText>47%</S.PercentageText>
+        </S.Percentage>
+        <ProfileCard />
+      </S.Header>
+      <S.Content>
+        <S.ContentText>{title}</S.ContentText>
+      </S.Content>
+    </S.Container>
+  );
+};
+
+export default memo(Card);

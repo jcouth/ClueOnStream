1ff947d69eb5d600aa075ff798c0508ed1ce9d84
Couth <craftcouth@gmail.com>
2022-09-29 00:51:05 -0300

    Add: inLobby ternary in cam

--- src/components/Cam/index.tsx
+++ src/components/Cam/index.tsx
@@ -9,31 +9,31 @@ import * as S from './styles';
 interface Props {
   isStreamerTurn: boolean;
   onSend(clue: string, amount: number): void;
-  inMenu?: boolean;
+  inLobby?: boolean;
 }
 
-const Cam: React.FC<Props> = ({ isStreamerTurn, onSend, inMenu = false }) => {
-  // const handleSettings = () => {};
+const Cam: React.FC<Props> = ({ isStreamerTurn, onSend, inLobby = false }) => {
+  const handleSettings = () => {};
 
-  // const handleLogOut = () => {};
+  const handleLogOut = () => {};
 
-  // const handleStart = () => {};
+  const handleStart = () => {};
 
-  // const renderInMenu = () => (
-  //   <>
-  //     <S.Button variant='secondary' onClick={handleSettings}>
-  //       <S.ButtonText>Configurações</S.ButtonText>
-  //     </S.Button>
-  //     <S.Buttons>
-  //       <S.Button variant='secondary' onClick={handleLogOut}>
-  //         <S.ButtonText>Deslogar</S.ButtonText>
-  //       </S.Button>
-  //       <S.Button variant='primary' onClick={handleStart}>
-  //         <S.ButtonText>Iniciar</S.ButtonText>
-  //       </S.Button>
-  //     </S.Buttons>
-  //   </>
-  // );
+  const renderLobby = () => (
+    <>
+      <S.Button variant='secondary' onClick={handleSettings}>
+        <S.ButtonText>Configurações</S.ButtonText>
+      </S.Button>
+      {/* <S.Buttons>
+        <S.Button variant='secondary' onClick={handleLogOut}>
+          <S.ButtonText>Deslogar</S.ButtonText>
+        </S.Button>
+        <S.Button variant='primary' onClick={handleStart}>
+          <S.ButtonText>Iniciar</S.ButtonText>
+        </S.Button>
+      </S.Buttons> */}
+    </>
+  );
 
   return (
     <S.Container>
@@ -41,7 +41,11 @@ const Cam: React.FC<Props> = ({ isStreamerTurn, onSend, inMenu = false }) => {
         <CameraIcon />
         <S.Title>Posicione sua câmera aqui</S.Title>
       </S.Header>
-      <Game isStreamerTurn={isStreamerTurn} onSend={onSend} />
+      {inLobby ? (
+        renderLobby()
+      ) : (
+        <Game isStreamerTurn={isStreamerTurn} onSend={onSend} />
+      )}
     </S.Container>
   );
 };

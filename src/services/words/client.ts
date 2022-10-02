4c823458c4c36113743133d3918f3f33cce29d43
Couth <craftcouth@gmail.com>
2022-10-02 17:58:19 -0300

    Update: api client of words

--- src/services/words/client.ts
+++ src/services/words/client.ts
@@ -2,17 +2,7 @@ import axios from 'axios';
 
 const { REACT_APP_WORD_BASE_URL } = process.env;
 
-const api = axios.create();
-
-api.defaults.baseURL = REACT_APP_WORD_BASE_URL;
-
-// api.defaults.headers = {
-//   'Access-Control-Allow-Origin': '*',
-//   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-//   'Access-Control-Allow-Headers':
-//     'X-Requested-With, Content-Type, Authorization',
-// };
-
-api.defaults.timeout = 2000;
-
-export { api };
+export const api = axios.create({
+  baseURL: REACT_APP_WORD_BASE_URL,
+  timeout: 2000,
+});
